c294f19c9fe9b762329d534cf751838f5881b163
Dileepoke <85066181+Dileepoke@users.noreply.github.com>
2022-06-22 21:31:38 +0530

    Create app.js

--- js/app.js
+++ js/app.js
@@ -140,7 +140,7 @@ function nav(path) {
       <li class="nav-item">
         <a class="nav-link" href="/${cur}:/"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-</svg> Home</a>
+</svg><span></span> Home</a>
       </li>`;
     var names = window.drive_names;
     var drive_name = window.drive_names[cur];
@@ -152,7 +152,7 @@ function nav(path) {
     names.forEach((name, idx) => {
         html += `<a class="dropdown-item"  href="/${idx}:/">${name}</a>`;
     });
-    html += `</div></li>`;
+    html += `</div></li>`; 
 
     html += `<li class="nav-item">
     <a class="nav-link" href="${UI.contact_link}" target="_blank">${UI.nav_link_4}</a>
